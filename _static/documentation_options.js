70fd9eaa79e541cd3c519f3a497e62e669282f91
Daniel Flook <daniel@flook.org>
2019-01-13 23:15:05 +0000

    Update docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.1.dev1+g906d32d',
+    VERSION: '2.0.1.dev2+g4e1c270',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
