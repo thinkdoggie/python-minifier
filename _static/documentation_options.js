59a77a6a99d48e3ea10576e1dfa96d748bff35e3
CircleCI <daniel@flook.org>
2019-11-18 20:03:28 +0000

    Update docs
    [skip ci]

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.3.1.dev1+g3e85bb1',
+    VERSION: '2.3.1.dev2+g452c64e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
